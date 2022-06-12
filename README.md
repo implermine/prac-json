2fedf94721dea4fb35ca3e54c53ac06b8cc760d0
이태강 <lkorea12345@gmail.com>
2022-06-12 18:09:25 +0900

    .

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# README.md
+# Ref
 
-fasterxml.jackson API를 사용합니다.  
-JSON marshalling - unmarshalling을 위 package로 사용해봅니다.
+fasterxml.jackson API를 사용합니다.
 
 기반 레퍼런스 문서 : https://www.baeldung.com/jackson
+
+# Summary
+
+* ObjectMapper의 Default 전략은 JSON 객체에는 값이 있고, Java 객체에는 없다면 unmarshalling 시 Exception이 발생한다.
+
+* ObjectMapper의 Default 전략은 JSON 객체에는 값이 없거나 null이고, Java 객체에는 있다면 null을 대응한다. 이때 primitiveType의 경우 0이 대입된다 (int의 경우)
+  이러한 default 전략을 configure를 통해 바꿀 수 있다.
--- src/test/java/org/example/CustomTest.java
+++ src/test/java/org/example/CustomTest.java
@@ -1,6 +1,8 @@
 package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +13,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 
 class CustomTest {
@@ -40,13 +44,16 @@ class CustomTest {
     // no-arg에 setter를 이용한다. (dto.Car 가보면 JsonField로 All-Arg로도 가능)
     public void test1() throws JsonProcessingException {
         String json = "{ \"name\" : \"Taegang\", \"length\" : \"182\" }";
-        Object1 object1 = objectMapper.readValue(json, Object1.class);
+
+        assertThrows(UnrecognizedPropertyException.class,() -> objectMapper.readValue(json,Object1.class));
 
         // com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field "name" (class org.example.CustomTest$Object1), not marked as ignorable (0 known properties: ])
         // setter 없으면 위 메시지처럼 없다고 나옴
+
+
         //com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field "length" (class org.example.CustomTest$Object1), not marked as ignorable (one known property: "name"])
         // setter 하나만 두면 (위에 주석친 세터처럼) 이런식으로 하나만 안다고 나옴
-        System.out.println(object1);
+//        System.out.println(object1);
     }
 
     @ToString
@@ -62,8 +69,10 @@ class CustomTest {
     @DisplayName("Deserialization할 때, 기본 생성자 없으면?")
     public void test2() throws JsonProcessingException {
         String json = "{ \"name\" : \"Taegang\", \"length\" : \"182\" }";
-        Object2 object2 = objectMapper.readValue(json, Object2.class);
-        System.out.println(object2);
+
+        assertThrows(MismatchedInputException.class,() -> objectMapper.readValue(json,Object2.class));
+
+//        System.out.println(object2);
 
         // com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot construct instance of `org.example.CustomTest$Object2` (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator)
         // at [Source: (String)"{ "name" : "Taegang", "length" : "182" }"; line: 1, column: 3]
