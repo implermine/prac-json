018692cca1c2603a43dadf65365525b31708f9dc
implermine <lkorea12345@gmail.com>
2022-09-08 17:13:59 +0900

    .

--- src/test/java/org/example/domain/hierarchy/Fish2.java
+++ src/test/java/org/example/domain/hierarchy/Fish2.java
@@ -3,10 +3,14 @@ package org.example.domain.hierarchy;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @JsonDeserialize(builder = Fish2.POJOBuilder.class)
+@Builder(builderClassName = "POJOBuilder")
+@Getter
 public class Fish2 {
 
 
@@ -14,47 +18,4 @@ public class Fish2 {
     private List<String> stringList;
 
     private String hey;
-
-    Fish2(List<String> stringList, String hey) {
-        this.stringList = stringList;
-        this.hey = hey;
-    }
-
-    public static POJOBuilder builder() {
-        return new POJOBuilder();
-    }
-
-    public List<String> getStringList() {
-        return this.stringList;
-    }
-
-    public String getHey() {
-        return this.hey;
-    }
-
-    public static class POJOBuilder {
-        private List<String> stringList;
-        private String hey;
-
-        POJOBuilder() {
-        }
-
-        public POJOBuilder stringList(List<String> stringList) {
-            this.stringList = stringList;
-            return this;
-        }
-
-        public POJOBuilder hey(String hey) {
-            this.hey = hey;
-            return this;
-        }
-
-        public Fish2 build() {
-            return new Fish2(stringList, hey);
-        }
-
-        public String toString() {
-            return "Fish2.POJOBuilder(stringList=" + this.stringList + ", hey=" + this.hey + ")";
-        }
-    }
 }
